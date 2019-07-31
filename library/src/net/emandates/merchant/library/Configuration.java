7c34d44893188a1a1751d497263010eac2ae6bfe
cristian.mihaita@maxcode.net <cristian.mihaita@maxcode.net>
2019-07-31 17:22:15 +0300

    Add alternate certificate key

--- library/src/net/emandates/merchant/library/Configuration.java
+++ library/src/net/emandates/merchant/library/Configuration.java
@@ -1,12 +1,9 @@
 package net.emandates.merchant.library;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
@@ -27,6 +24,7 @@ public class Configuration {
     private String signingCertificateAlias;
     private String signingCertificatePassword;
     private String acquirerCertificateAlias;
+    private String acquirerAlternateCertificateAlias;
     private String acquirerUrl_DirectoryReq;
     private String acquirerUrl_TransactionReq;
     private String acquirerUrl_StatusReq;
@@ -53,6 +51,101 @@ public class Configuration {
 
     private Configuration() {
     }
+    
+    /**
+     * Constructor that highlights all required fields for this object.
+     * @param eMandateContractId eMandate.ContractID as supplied to you by the creditor bank
+     * @param eMandateContractSubId eMandate.ContractSubId as supplied to you by the creditor bank
+     * @param merchantReturnUrl A valid URL to which the debtor banks redirects to, after the debtor has authorized a transaction.
+     * @param keyStoreLocation A Java keystore (file on the disk) that stores the certificates
+     * @param keyStorePassword The password used to access the keystore
+     * @param signingCertificateAlias A string which specifies the alias of the certificate to use to sign messages to the creditor bank.
+     * @param signingCertificatePassword The password of the private key of the signing certificate
+     * @param acquirerCertificateAlias A string which specifies the alias of the certificate to use to validate messages from the creditor
+     * bank.
+     * @param acquirerAlternateCertificateAlias A string which specifies the alias of the alternate certificate to use to validate messages 
+     * from the creditor bank.
+     * @param acquirerUrl_DirectoryReq The URL to which the library sends Directory request messages
+     * @param acquirerUrl_TransactionReq The URL to which the library sends Transaction messages (including eMandates messages).
+     * @param acquirerUrl_StatusReq The URL to which the library sends Status request messages.
+     * @param logsEnabled This tells the library that it should output debug logging messages.
+     * @param serviceLogsEnabled This tells the library that it should save ISO pain raw messages or not. Default is true.
+     * @param serviceLogsLocation A directory on the disk where the library saves ISO pain raw messages.
+     * @param serviceLogsPattern A string that describes a pattern to distinguish the ISO pain raw messages.
+     * @param loggerFactory ILoggerFactory instance that is used to create ILogger object.
+     */
+    public Configuration(String eMandateContractId, int eMandateContractSubId, String merchantReturnUrl, String keyStoreLocation,
+            String keyStorePassword, String signingCertificateAlias, String signingCertificatePassword, String acquirerCertificateAlias, String acquirerAlternateCertificateAlias,
+            String acquirerUrl_DirectoryReq, String acquirerUrl_TransactionReq, String acquirerUrl_StatusReq, boolean logsEnabled, boolean serviceLogsEnabled,
+            String serviceLogsLocation, String serviceLogsPattern, ILoggerFactory loggerFactory) throws IOException {
+        
+        this(
+            eMandateContractId, 
+            eMandateContractSubId, 
+            merchantReturnUrl, 
+            loadKeyStore(keyStoreLocation), // load the InputStream
+            keyStorePassword, 
+            signingCertificateAlias, 
+            signingCertificatePassword, 
+            acquirerCertificateAlias,
+            acquirerAlternateCertificateAlias,
+            acquirerUrl_DirectoryReq, 
+            acquirerUrl_TransactionReq, 
+            acquirerUrl_StatusReq, 
+            logsEnabled, 
+            serviceLogsEnabled, 
+            serviceLogsLocation, 
+            serviceLogsPattern, 
+            loggerFactory);
+    }
+    
+    /**
+     * Constructor that highlights all required fields for this object.
+     * @param eMandateContractId eMandate.ContractID as supplied to you by the creditor bank
+     * @param eMandateContractSubId eMandate.ContractSubId as supplied to you by the creditor bank
+     * @param merchantReturnUrl A valid URL to which the debtor banks redirects to, after the debtor has authorized a transaction.
+     * @param keyStore A Java keystore InputStream that stores the certificates
+     * @param keyStorePassword The password used to access the keystore
+     * @param signingCertificateAlias A string which specifies the alias of the certificate to use to sign messages to the creditor bank.
+     * @param signingCertificatePassword The password of the private key of the signing certificate
+     * @param acquirerCertificateAlias A string which specifies the alias of the certificate to use to validate messages from the creditor
+     * bank.
+     * @param acquirerAlternateCertificateAlias A string which specifies the alias of the alternate certificate to use to validate messages 
+     * from the creditor bank.
+     * @param acquirerUrl_DirectoryReq The URL to which the library sends Directory request messages
+     * @param acquirerUrl_TransactionReq The URL to which the library sends Transaction messages (including eMandates messages).
+     * @param acquirerUrl_StatusReq The URL to which the library sends Status request messages.
+     * @param logsEnabled This tells the library that it should output debug logging messages.
+     * @param serviceLogsEnabled This tells the library that it should save ISO pain raw messages or not. Default is true.
+     * @param serviceLogsLocation A directory on the disk where the library saves ISO pain raw messages.
+     * @param serviceLogsPattern A string that describes a pattern to distinguish the ISO pain raw messages.
+     * @param loggerFactory ILoggerFactory instance that is used to create ILogger object.
+     */
+    public Configuration(String eMandateContractId, int eMandateContractSubId, String merchantReturnUrl, InputStream keyStore,
+            String keyStorePassword, String signingCertificateAlias, String signingCertificatePassword, String acquirerCertificateAlias, String acquirerAlternateCertificateAlias,
+            String acquirerUrl_DirectoryReq, String acquirerUrl_TransactionReq, String acquirerUrl_StatusReq, boolean logsEnabled, boolean serviceLogsEnabled,
+            String serviceLogsLocation, String serviceLogsPattern, ILoggerFactory loggerFactory) throws IOException {
+
+                this.eMandateContractId = eMandateContractId;
+        this.eMandateContractSubId = eMandateContractSubId;
+        this.merchantReturnUrl = merchantReturnUrl;
+        this.signingCertificateAlias = signingCertificateAlias;
+        this.signingCertificatePassword = signingCertificatePassword;
+        this.acquirerCertificateAlias = acquirerCertificateAlias;
+        this.acquirerAlternateCertificateAlias = acquirerAlternateCertificateAlias;
+        this.acquirerUrl_DirectoryReq = acquirerUrl_DirectoryReq;
+        this.acquirerUrl_TransactionReq = acquirerUrl_TransactionReq;
+        this.acquirerUrl_StatusReq = acquirerUrl_StatusReq;
+        this.logsEnabled = logsEnabled;
+        this.serviceLogsEnabled = serviceLogsEnabled;
+        this.serviceLogsLocation = serviceLogsLocation;
+        this.serviceLogsPattern = serviceLogsPattern;
+        this.tls12Enabled = false;
+        this.loggerFactory = loggerFactory != null ? loggerFactory : new LoggerFactory();
+
+        setKeyStoreAndPass(keyStore, keyStorePassword);
+    }
+    
 
     /**
      * Constructor that highlights all required fields for this object.
@@ -281,6 +374,7 @@ public class Configuration {
         setSigningCertificateAlias(getConfigValue(doc, "eMandates.SigningCertificate.Alias"));
         setSigningCertificatePassword(getConfigValue(doc, "eMandates.SigningCertificate.Password"));
         setAcquirerCertificateAlias(getConfigValue(doc, "eMandates.AcquirerCertificate.Alias"));
+        setAcquirerAlternateCertificateAlias(getConfigValue(doc, "eMandates.AcquirerAlternateCertificate.Alias"));
         setAcquirerUrl_DirectoryReq(getConfigValue(doc, "eMandates.Acquirer.DirectoryRequestUrl"));
         setAcquirerUrl_TransactionReq(getConfigValue(doc, "eMandates.Acquirer.TransactionRequestUrl"));
         setAcquirerUrl_StatusReq(getConfigValue(doc, "eMandates.Acquirer.StatusRequestUrl"));
@@ -308,6 +402,7 @@ public class Configuration {
         setSigningCertificateAlias(values.getSigningCertificateAlias());
         setSigningCertificatePassword(values.getSigningCertificatePassword());
         setAcquirerCertificateAlias(values.getAcquirerCertificateAlias());
+        setAcquirerAlternateCertificateAlias(values.getAcquirerAlternateCertificateAlias());
         setAcquirerUrl_DirectoryReq(values.getAcquirerUrl_DirectoryReq());
         setAcquirerUrl_TransactionReq(values.getAcquirerUrl_TransactionReq());
         setAcquirerUrl_StatusReq(values.getAcquirerUrl_StatusReq());
@@ -395,6 +490,20 @@ public class Configuration {
     public void setAcquirerCertificateAlias(String acquirerCertificateAlias) {
         this.acquirerCertificateAlias = acquirerCertificateAlias;
     }
+    
+    /**
+     * @param A string which specifies the alias of the alternate certificate to validate  received messages from the creditor bank
+     */
+    public String getAcquirerAlternateCertificateAlias() {
+		return acquirerAlternateCertificateAlias;
+	}
+    
+    /**
+     * @param acquirerAlternateCertificateAlias A string which specifies the alias of the alternate certificate to validate received messages from the creditor bank
+     */
+	public void setAcquirerAlternateCertificateAlias(String acquirerAlternateCertificateAlias) {
+		this.acquirerAlternateCertificateAlias = acquirerAlternateCertificateAlias;
+	}
 
     /**
      * @return The URL to which the library sends Directory request messages
@@ -484,7 +593,7 @@ public class Configuration {
     public void setKeyStoreAndPass(InputStream keyStore, String keyStorePassword) throws IOException {
         this.keyStorePassword = keyStorePassword;
 
-        this.keyStore = keyStore;
+        this.keyStore = markSupported(keyStore);
         this.keyStore.mark(Integer.MAX_VALUE);
     }
 
@@ -626,11 +735,18 @@ public class Configuration {
         if (url == null) {
             url = Configuration.class.getClassLoader().getResource(storeLocation);
         }
-        InputStream keyStore = url.openStream();
-        keyStore.mark(Integer.MAX_VALUE);
-        return keyStore;
-    }
-    
+		InputStream keyStore = markSupported(url.openStream());
+		keyStore.mark(Integer.MAX_VALUE);
+		return keyStore;
+	}
+
+	private static InputStream markSupported(InputStream in) {
+		if (in.markSupported()) {
+			return in;
+		}
+		return new BufferedInputStream(in);
+	}
+          
     private String getConfigValue(Document doc, String key) {
         NodeList nl = doc.getElementsByTagName("add");
         for (int i = 0; i < nl.getLength(); i++) {
